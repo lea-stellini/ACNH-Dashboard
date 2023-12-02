effad302e6b7cd8f51ce233ce1e72a0b5a0effd6
lea-stellini <lea.stellini@gmail.com>
2023-12-02 17:14:33 +0100

    update style

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,17 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FaPaintBrush, FaHome } from 'react-icons/fa';
 import './Navbar.css';
 
 function Navbar() {
+    
+    const url = useLocation().pathname;
 
     return(
         <div className='navbar'>
-            <Link to='/' className='icons home'>
+            <Link to='/' className={`icons ${url === "/" ? "active" : null}`}>
                 <FaHome />
             </Link>
-            <Link to='/art' className='icons art'>
+            <Link to='/art' className={`icons ${url === "/art" ? "active" : null}`}>
                 <FaPaintBrush />
             </Link>
 
